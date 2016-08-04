8e45509e014d3261e5296e904813603fa1b3688d
acobrelo <acobrelo@gmail.com>
2016-08-04 15:20:35 -0400

    Deleting and updating items completed
    All that's left to fix is style and pathing for now, will add more
    features later.

--- app/components/notebook-main/task-store/component.js
+++ app/components/notebook-main/task-store/component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['done'],
+  done: Ember.computed.alias('item.done'),
 
   actions: {
     click () {
@@ -14,6 +16,8 @@ export default Ember.Component.extend({
       let item = this.get('item');
       item.destroyRecord();
       console.log("gone?");
-    }
+    },
+
+    store: Ember.inject.service(),
   }
 });
